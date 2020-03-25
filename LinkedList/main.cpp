c629b8cd3478a39774f8d7da737aaf83f39cd5a2
Sara <stagarot@trinity.edu>
2020-03-25 09:55:56 -0500

    Initial Commit

---
+++ LinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedList
+//
+//  Created by Sara Tagarot on 3/25/20.
+//  Copyright © 2020 DataAbs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
